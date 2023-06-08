d06c4c4f027d3b555f6061f75e3729a7f4d572aa
Ethaniel-Ravanello <ravaneloe@gmail.com>
2023-06-08 23:35:28 +0700

    feat: Making the table at the front end side

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        background: "#15191d",
+        text: "#F8F8F2",
+        accent: "#212529",
+        table: "#313b4b",
+        button: "#6f6f6f",
+        accents: "#262626",
       },
     },
   },
   plugins: [],
-}
+};
